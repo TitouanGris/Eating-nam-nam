6fcf7b8656db83b1e1e481e4773b3e658b6800d9
Guillaume Lebeau <guillaume.lebeau44@hotmail.fr>
2023-12-13 11:34:41 +0100

    création managers

--- backend/src/models/TagsManager.js
+++ backend/src/models/TagsManager.js
@@ -5,7 +5,7 @@ class TagsManager extends AbstractManager {
     super({ table: "tags" });
   }
 
-  async create({ categoryId, imageUrl, name }) {
+  async create(categoryId, imageUrl, name) {
     const [result] = await this.database.query(
       `INSERT INTO ${this.table} (category_id, image_url, name) values (?, ?, ?)`,
       [categoryId, imageUrl, name]
@@ -14,7 +14,7 @@ class TagsManager extends AbstractManager {
     return [result];
   }
 
-  async update({ id, categoryId, imageUrl, name }) {
+  async update(id, categoryId, imageUrl, name) {
     const [result] = await this.database.query(
       `UPDATE ${this.table} SET category_id=?, image_url=?, name=? WHERE id = ? `,
       [id, categoryId, imageUrl, name]
