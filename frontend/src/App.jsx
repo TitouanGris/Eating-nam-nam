c213bf13878153c1064b75e1b9dc44e59d6f5b6d
hlnvrnt <helene.vernet@live.fr>
2023-12-13 15:26:47 +0100

    none

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,4 @@
 import { useEffect, useState } from "react";
-import Counter from "./components/Counter";
-import logo from "./assets/logo.svg";
-
 import "./App.css";
 
 function App() {
@@ -15,49 +12,20 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React !</p>
-        {recipe.map((r) => {
-          return (
-            <>
-              <div style={{ width: "100px", height: "100px", color: "white" }}>
-                {" "}
-                {r.name}
-              </div>
-              <div style={{ width: "100px", height: "100px", color: "white" }}>
-                {" "}
-                {r.summary}
-              </div>
-            </>
-          );
-        })}
-
-        <Counter />
-
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      {recipe.map((r) => {
+        return (
+          <>
+            <div style={{ width: "100px", height: "100px", color: "black" }}>
+              {" "}
+              {r.name}
+            </div>
+            <div style={{ width: "100px", height: "100px", color: "black" }}>
+              {" "}
+              {r.summary}
+            </div>
+          </>
+        );
+      })}
     </div>
   );
 }
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function Home() {
+  return (
+    <div className="home-container">
+      <img src="/images/logo.png" alt="logo" />
+      <img src="/images/home-img.jpg" alt="img-accueil" />
+      <div className="home-buttons">
+        <NavLink>
+          <button type="button">Démarrer</button>
+        </NavLink>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
