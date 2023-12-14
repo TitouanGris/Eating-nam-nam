7feef0a159aa297fd195eecfbd2b8e32f6ac802d
Guillaume Lebeau <guillaume.lebeau44@hotmail.fr>
2023-12-14 15:19:15 +0100

    first draft of navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+// import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  const [isConnected, setIsConnected] = useState(false);
-  const handleConnected = () => {
-    setIsConnected(!isConnected);
-  };
+  // const [isConnected, setIsConnected] = useState(false);
+  // const handleConnected = () => {
+  //   setIsConnected(!isConnected);
+  // };
 
   return (
     <div className="NavBar">
@@ -14,42 +14,28 @@ function NavBar() {
           <p>Accueil</p>
         </button>
       </NavLink>
-      <NavLink>
-        <button
-          type="button"
-          disabled={!isConnected}
-          className="publish_button"
-        >
-          <img alt="publish" src="./src/assets/images/add.png" />
-          <p>Publier</p>
-        </button>
-      </NavLink>
-      <NavLink>
-        <button
-          type="button"
-          className={`account${isConnected ? "_connected" : ""}`}
-          onClick={handleConnected}
-        >
-          <img alt="account" src="./src/assets/images/account.png" />
-          <p>{isConnected === false ? "Créer un compte" : "Profil"}</p>
-        </button>
-      </NavLink>
-      <NavLink>
+      {/* <button type="button" disabled={!isConnected} className="publish_button">
+        <img alt="publish" src="./src/assets/images/add.png" />
+        <p>Publier</p>
+      </button> */}
+      {/* <button
+        type="button"
+        className={`account${isConnected ? "_connected" : ""}`}
+        onClick={handleConnected}
+      >
+        <img alt="account" src="./src/assets/images/account.png" />
+        <p>{isConnected === false ? "Créer un compte" : "Profil"}</p>
+      </button> */}
+      <NavLink to="/filters">
         <button type="button" className="filter_button">
           <img alt="filters" src="./src/assets/images/settings.png" />
           <p>Filtres</p>
         </button>
       </NavLink>
-      <NavLink>
-        <button
-          type="button"
-          disabled={!isConnected}
-          className="favorite_button"
-        >
-          <img alt="favorite" src="./src/assets/images/heartFill.png" />
-          <p>Favoris</p>
-        </button>
-      </NavLink>
+      {/* <button type="button" disabled={!isConnected} className="favorite_button">
+        <img alt="favorite" src="./src/assets/images/heartFill.png" />
+        <p>Favoris</p>
+      </button> */}
     </div>
   );
 }
