d528965d4b2e7b9daf692c96a84deddc2254665d
Nadine <nadine.motel@gmail.com>
2023-12-19 15:40:10 +0100

    polish page details recette et modification des images

--- backend/src/router.js
+++ backend/src/router.js
@@ -11,6 +11,7 @@ const itemControllers = require("./controllers/itemControllers");
 const recipeControllers = require("./controllers/recipeControllers");
 const tagsControllers = require("./controllers/tagsControllers");
 const stepControllers = require("./controllers/stepControllers");
+const ingredientControllers = require("./controllers/ingredientControllers");
 
 // Route to get a list of items
 router.get("/items", itemControllers.browse);
@@ -18,6 +19,7 @@ router.get("/recipe", recipeControllers.browse);
 router.get("/recipe/:id", recipeControllers.read);
 router.get("/step/:id", stepControllers.readSteps);
 router.get("/tags", tagsControllers.browse);
+router.get("/ingredients/:id", ingredientControllers.readIngredientsByRecipeId);
 
 // Route to get a specific item by ID
 router.get("/items/:id", itemControllers.read);
--- frontend/src/pages/RecipeDetails.jsx
+++ frontend/src/pages/RecipeDetails.jsx
@@ -1,31 +1,81 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useParams, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
-import RecipeCard from "../components/RecipeCard";
 
 function RecipeDetails() {
   const recipe = useLoaderData();
   const [steps, setSteps] = useState([]);
+  const [ingredients, setIngredients] = useState([]);
+  const { id } = useParams();
 
   useEffect(() => {
-    fetch("http://localhost:3310/api/step/1")
+    fetch(`http://localhost:3310/api/step/${id}`)
       .then((res) => res.json())
       .then((data) => setSteps(data));
   }, []);
 
+  useEffect(() => {
+    fetch(`http://localhost:3310/api/ingredients/${id}`)
+      .then((res) => res.json())
+      .then((data) => setIngredients(data));
+  }, []);
+
   return (
-    <div className="recipeCard">
-      <RecipeCard r={recipe} key={recipe.id} />
-      <p className="recipe_summary">{recipe.summary}</p>
-      <div className="steps_details">
+    <div className="recipeDetails">
+      <div className="recipeDetailsHeader">
+        <Link to="http://localhost:3000/browse">
+          <img src="/src/assets/images/back.png" alt="Back Arrow" />
+        </Link>
+        <h2>{recipe.recipeName}</h2>
+      </div>
+      <div className="imgContainer">
+        <img
+          src={`http://localhost:3310${recipe.recipeImage}`}
+          alt={`${recipe.recipeName}`}
+        />
+      </div>
+      <div className="card">
+        <div className="tags">
+          <div className="price">
+            <img
+              src={`http://localhost:3310${recipe.tagPriceUrl}`}
+              alt="r.TagPrice"
+            />
+          </div>
+          <div className="difficulty">
+            <img
+              src={`http://localhost:3310${recipe.tagDifficultyUrl}`}
+              alt="r.TagDifficulty"
+            />
+          </div>
+          <div className="serving">
+            <img
+              src="/src/assets/images/nbServingImage.png"
+              alt="recipeServing"
+            />
+            <p>{recipe.recipeServing}</p>
+          </div>
+          <div className="duration">
+            <img src="/src/assets/images/durationImage.png" alt="TagDuration" />
+            <p>{recipe.tagDuration}</p>
+          </div>
+        </div>
+      </div>
+      <h4 className="recipe_summary">{recipe.summary}</h4>
+      <div className="recipe_ingredients">
+        <h3>Ingrédients</h3>
         <ul>
-          {steps.map((step) => {
-            return (
-              <li key={step.id}>
-                {step.id}. {step.description}
-              </li>
-            );
-          })}
+          {ingredients.map((ingredient) => (
+            <li>{`${ingredient.quantity} ${ingredient.unitName} ${ingredient.ingredientName}`}</li>
+          ))}
         </ul>
+      </div>
+      <div className="steps_details">
+        <h3>Recette</h3>
+        <ol>
+          {steps.map((step) => {
+            return <li key={step.step_number}>{step.description}</li>;
+          })}
+        </ol>
       </div>
     </div>
   );
